b187346a82b9ebeb48312c4858f9d2c0d09fb175
Mark Helt <heltmm@gmail.com>
2017-11-17 10:07:58 -0800

    add devise and will_pageinate

---
+++ db/migrate/20171117180004_add_admin_and_name_column.rb
@@ -0,0 +1,7 @@
+class AddAdminAndNameColumn < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+  end
+end
