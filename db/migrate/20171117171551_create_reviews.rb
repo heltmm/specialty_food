03ae9710f431c3f916e658a3754ece8782e5a565
Mark Helt <heltmm@gmail.com>
2017-11-17 09:31:43 -0800

    update models, create specs, create routes

--- db/migrate/20171117171551_create_reviews.rb
+++ db/migrate/20171117171551_create_reviews.rb
@@ -4,6 +4,9 @@ class CreateReviews < ActiveRecord::Migration[5.1]
       t.column :author, :string
       t.column :content_body, :string
       t.column :rating, :integer
+      t.column :product_id, :integer
+
+      t.timestamps
     end
   end
 end
