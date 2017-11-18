a8dae7bed04ee8b881d43fdfc4dccb562dbbb333
Mark Helt <heltmm@gmail.com>
2017-11-17 16:56:29 -0800

    add functinality to update products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,9 +28,22 @@ def show
 end
 
 def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  redirect_to products_path
+end
+
+def edit
+  @product = Product.find(params[:id])
+end
+
+def update
+  @product = Product.find(params[:id])
+  if @product.update(product_params)
     redirect_to products_path
+  else
+    render :edit
+  end
 end
 
 private
