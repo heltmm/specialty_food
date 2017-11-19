700f337e441e29c192574d825e3b760fb7782668
Mark Helt <heltmm@gmail.com>
2017-11-18 17:52:43 -0800

    validates content between 50-250 characters, validates rating between 1,5

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,10 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :content_body, :presence => true
+  validates :content_body, length: { in: 50..250 }
   validates :rating, :presence => true
+  validates :rating, :inclusion => 1..5
+
 
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
 end
